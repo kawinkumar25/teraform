095fd1e7641d909a29f16a7138341058abc1a687
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2024-01-17 11:18:20 +0530

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -14,5 +14,6 @@ echo "haiiiiiiiiiiiiiiiiiiiiiiiiii"
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "haiiiiiiiiiiiiiiiiiiiiiiiiii"
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+echo "haiiiiiiiiiiiiiiiiiiiiiiiiii"
 apt-get -y update
 apt-get -y install google-chrome-stable
