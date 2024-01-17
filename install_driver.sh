fec3578e2789b18a200cc470b9bd6ee39a8c9d65
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2024-01-17 12:56:24 +0530

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -1,10 +1,16 @@
 # Download and Install chromedriver
-# wget -N https://chromedriver.storage.googleapis.com/73.0.3683.80/chromedriver_linux64.zip -P ~/
-wget -N https://chromedriver.storage.googleapis.com/71.0.3578.80/chromedriver_linux64.zip -P ~/
+# # wget -N https://chromedriver.storage.googleapis.com/73.0.3683.80/chromedriver_linux64.zip -P ~/
+# wget -N https://chromedriver.storage.googleapis.com/71.0.3578.80/chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip -P ~/
+
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 echo "hiiiiiiiiiiiiiiiiiiiiiii"
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+
+# List files in /usr/local/bin/
+ls -la /usr/local/bin/
+echo "hiiiiiiiiiiiiiiiiiiiiiii"
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
 
